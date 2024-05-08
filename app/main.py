8fc5c54cbe4bccd481a3d398f05edd2cdd6097a1
kaifkh20 <kaifkhan.biz@gmail.com>
2024-05-09 00:30:35 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,11 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn,adrr = server_socket.accept() # wait for client
 
-    if conn:
-        conn.sendall("PONG")
+    pong = "+PONG\r\n"
+
+    with conn:
+        conn.recv(1024)
+        conn.send(pong.encode())
         
 
 
